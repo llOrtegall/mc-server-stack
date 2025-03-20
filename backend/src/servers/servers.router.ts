2e4076003029534015bd8502e7b1945c9f3db046
llortegall <ivanortega_97@hotmail.com>
2025-03-20 11:05:00 -0500

    style(backend): apply biome formatting to servers module

--- backend/src/servers/servers.router.ts
+++ backend/src/servers/servers.router.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express'
-import * as serversController from './servers.controller.js'
+import { Router } from 'express';
+import * as serversController from './servers.controller.js';
 
-export const serversRouter = Router()
+export const serversRouter = Router();
 
-serversRouter.get('/', serversController.list)
-serversRouter.post('/', serversController.create)
-serversRouter.get('/:id', serversController.get)
-serversRouter.delete('/:id', serversController.remove)
-serversRouter.post('/:id/start', serversController.start)
-serversRouter.post('/:id/stop', serversController.stop)
-serversRouter.post('/:id/restart', serversController.restart)
+serversRouter.get('/', serversController.list);
+serversRouter.post('/', serversController.create);
+serversRouter.get('/:id', serversController.get);
+serversRouter.delete('/:id', serversController.remove);
+serversRouter.post('/:id/start', serversController.start);
+serversRouter.post('/:id/stop', serversController.stop);
+serversRouter.post('/:id/restart', serversController.restart);
