4964b9c5c385fc396473d3c030d2ca14116b27d6
llortegall <ivanortega_97@hotmail.com>
2026-06-04 12:30:00 -0500

    feat(backup): route the backup schedule endpoints

--- backend/src/modules/backup/interface/backup.router.ts
+++ backend/src/modules/backup/interface/backup.router.ts
@@ -5,5 +5,7 @@ export const backupRouter = Router({ mergeParams: true });
 
 backupRouter.get('/', backupController.list);
 backupRouter.post('/', backupController.create);
+backupRouter.get('/schedule', backupController.getSchedule);
+backupRouter.put('/schedule', backupController.saveSchedule);
 backupRouter.delete('/:backupId', backupController.remove);
 backupRouter.post('/:backupId/restore', backupController.restore);
